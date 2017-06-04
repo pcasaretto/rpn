3e9f0fd2e7c1ad463d6a3c4975cb83a94ef1af32
Paulo Casaretto <pcasaretto@gmail.com>
2017-06-03 22:45:34 -0700

    Preliminary interface

--- cmd/rpn/rpn.go
+++ cmd/rpn/rpn.go
@@ -1,34 +1,105 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
 	"strconv"
+	"strings"
 
-	"github.com/pcasaretto/rpn"
+	"github.com/jroimartin/gocui"
+	"github.com/pcasaretto/rpn/internal/rpn"
 )
 
+var stack *rpn.Stack
+
 func main() {
-	stack := rpn.NewStack()
-	scanner := bufio.NewScanner(os.Stdout)
-	// Set the split function for the scanning operation.
-	scanner.Split(bufio.ScanWords)
-	for scanner.Scan() {
-		text := scanner.Text()
-		if text == "+" {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	stack = rpn.NewStack()
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+	// scanner := bufio.NewScanner(os.Stdout)
+	// // Set the split function for the scanning operation.
+	// scanner.Split(bufio.ScanWords)
+	// for scanner.Scan() {
+	// 	text := scanner.Text()
+	// 	log.Println(stack.Contents)
+	// }
+	// if err := scanner.Err(); err != nil {
+	// 	fmt.Fprintln(os.Stderr, "reading input:", err)
+	// }
+}
+
+var stackView, editorView *gocui.View
+
+func layout(g *gocui.Gui) (err error) {
+	_, maxY := g.Size()
+	if stackView, err = g.SetView("stack", 0, 0, 80, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return
+		}
+	}
+	if editorView, err = g.SetView("input", 0, maxY-20, 80, 35); err != nil {
+		if err != gocui.ErrUnknownView {
+			return
+		}
+		if _, err = g.SetCurrentView("input"); err != nil {
+			return
+		}
+		editorView.Wrap = true
+		editorView.Editor = singleLineEditor
+		editorView.Editable = true
+	}
+	return
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+var singleLineEditor gocui.Editor = gocui.EditorFunc(func(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	if key == gocui.KeyEnter {
+		line := v.Buffer()
+
+		line = strings.Replace(line, "\x00", "", -1)
+		line = strings.Replace(line, "\n", "", -1)
+		line = strings.Replace(line, " ", "", -1)
+
+		if line == "+" {
 			rpn.Add(stack)
 		} else {
-			f, err := strconv.ParseFloat(text, 64)
+			f, err := strconv.ParseFloat(line, 64)
 			if err != nil {
 				panic(err)
 			}
 			stack.Push(f)
 		}
-		log.Println(stack.Contents)
+		updateStackView()
+
+		v.Clear()
+		v.SetCursor(0, 0)
+		v.SetOrigin(0, 0)
+		return
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading input:", err)
+	gocui.DefaultEditor.Edit(v, key, ch, mod)
+})
+
+func updateStackView() {
+	stackView.Clear()
+	stackView.SetCursor(0, 0)
+	stackView.SetOrigin(0, 0)
+	for _, f := range stack.Contents {
+		fmt.Fprintln(stackView, f)
 	}
 }
